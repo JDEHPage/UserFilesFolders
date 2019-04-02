43bd8d2c7b864a766896bd1c80bc7d3071a03f1f
John Page <jdehpage@gmail.com>
2019-04-02 15:05:19 +0100

    UserFilesFolders excercise

---
+++ UsersFilesFolders/src/main/java/com/codeclan/example/UsersFilesFolders/UsersFilesFoldersApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.UsersFilesFolders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersFilesFoldersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersFilesFoldersApplication.class, args);
+	}
+
+}
